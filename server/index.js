eafc1eb540ec048f41ee23f24d7bbec9272789e6
prepat21 <88353973+prepat21@users.noreply.github.com>
2023-01-09 15:50:41 -0600

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const colors = require("colors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
-const colors = require("colors");
 const cors = require("cors");
 const schema = require("./schema/schema.js");
-const helmet = require("helmet");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const port = process.env.PORT || 5000;
+
+const helmet = require("helmet");
 
 const app = express();
 
@@ -17,18 +19,19 @@ connectDB();
 app.use(cors());
 
 app.use(
-  "/graphql",
+  "/graphql?",
   graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === "production",
   })
 );
 
+app.use(express.static("public"));
 
-app.use(
-  helmet.contentSecurityPolicy({
-    direcive:{defaultSrc:["'self'"],}}
-  )
-);
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__direname, "public", "index.html"));
+});
 
-app.listen(port, ()=>{console.log(`Server running on port ${port}`)});
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
