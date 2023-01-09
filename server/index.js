18cd411de37ad3e12621d00a09b4a54161de99f4
prepat21 <88353973+prepat21@users.noreply.github.com>
2023-01-09 01:00:57 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const { graphqlHTTP } = require("express-graphql");
 const colors = require("colors");
 const cors = require("cors");
 const schema = require("./schema/schema.js");
-const helmet = require('fusion-plugin-react-helmet-async');
+const helmet = require("helmet");
 const connectDB = require("./config/db");
 
 const port = process.env.PORT || 5000;
@@ -16,7 +16,14 @@ connectDB();
 
 app.use(cors());
 
-app.use(helmet({ contentSecurityPolicy: false, crossOriginEmbedderPolicy: false }))
+app.use(
+  "/graphql?",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "production",
+  })
+);
+
 
 app.use(
   "/graphql",
