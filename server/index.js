70e8e73ad343c24e040190a036cd9ae9930046da
prepat21 <88353973+prepat21@users.noreply.github.com>
2023-01-09 22:24:48 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,12 +26,6 @@ app.use(
   })
 );
 
-app.use(express.static("public"));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
-});
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
